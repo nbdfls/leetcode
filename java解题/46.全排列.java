29d2a40995cab25b9e985ecafacbc938edb968c0
nbdfls <250015382@qq.com>
2021-02-07 15:04:34 +0800

    递归回溯

--- java解题/46.全排列.java
+++ java解题/46.全排列.java
@@ -1,14 +1,31 @@
 /*
- * @lc app=leetcode.cn id=46 lang=java
- *
- * [46] 全排列
+ * @Author: fanfan
  */
-
-// @lc code=start
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+        List<Integer> temp = new ArrayList<>();
+        boolean[] used = new boolean[len];
+        dfs(nums,temp,0,res,used);
+        
+        return res;
+    }
+    public void dfs(int[] nums,List<Integer> temp,int index,List<List<Integer>> res,boolean[]used){
+        if(index >= nums.length){
+            res.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        for(int i = 0;i<nums.length;i++){
+            if(used[i]==false){
+                temp.add(nums[i]);
+                used[i]=true;
+                dfs(nums, temp, index+1, res, used);
+                used[i]=false;
+                temp.remove(temp.size()-1);
+            }
+
+        }
 
     }
 }
-// @lc code=end
-
